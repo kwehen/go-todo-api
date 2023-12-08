e9f57547826cc2f77a1486cbfe2f84fb4d572d47
kwehen <kwesihenry11@gmail.com>
2023-12-07 23:29:48 -0500

    lots needs to change

--- database/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tasks (
-    task_id SERIAL PRIMARY KEY,
-    task varchar(256) NOT NULL,
-    urgency varchar(10) NOT NULL DEFAULT 'Low',
-    hours numeric(3,1), 
-    completed boolean NOT NULL DEFAULT false
-);
