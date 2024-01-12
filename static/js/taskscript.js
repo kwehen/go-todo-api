9a1f427723a3fa3fdd918dac422afe8f01e853ef
kwehen <kwesihenry11@gmail.com>
2024-01-11 20:14:03 -0500

    added drilling down into tasks

--- static/js/taskscript.js
+++ static/js/taskscript.js
@@ -29,3 +29,11 @@ document.getElementById('completeThisTask').addEventListener('submit', function(
         }
     };
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.task-row').forEach(row => {
+        row.addEventListener('click', function() {
+            window.location.href = '/tasks/' + this.dataset.id;
+        });
+    });
+});
